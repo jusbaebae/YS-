7fa6bf73e1d8637a617a91286686e24904494be6
yallo428 <yallo428@naver.com>
2023-10-10 23:00:52 +0900

    캐릭터 픽창 구현

---
+++ YS-/Assets/Scripts/MainScript/Selection.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharScript : MonoBehaviour
+{
+
+    [SerializeField] 
+    void Update()
+    {
+        
+    }
+}
