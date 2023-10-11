cfad18e156f7ab8793dae7dd72a57ded6d67666a
yallo428 <116006875+yallo428@users.noreply.github.com>
2023-10-11 16:10:47 +0900

    22

---
+++ YS-/Assets/Scripts/MainScript/CharData.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharData : MonoBehaviour
+{
+    [SerializeField] Sprite portrait;
+    [SerializeField] Sprite info_img;
+
+    float hp;
+    float attack;
+    int speed;
+}
--- YS-/Assets/Scripts/MainScript/Selection.cs
+++ YS-/Assets/Scripts/MainScript/Selection.cs
@@ -1,13 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class CharScript : MonoBehaviour
 {
 
-    [SerializeField] 
+    [SerializeField] GameObject infoImage;
+
+    private Sprite charImage;
+    private GameObject character;
+
+   
     void Update()
     {
-        
+
+    }
+
+    public void CharPIck()
+    {
+        character = EventSystem.current.currentSelectedGameObject;
+        charImage = character.transform.GetChild(0).gameObject.GetComponent<Image>().sprite;
+
+        infoImage.GetComponent<Image>().sprite = charImage;
+
     }
 }
