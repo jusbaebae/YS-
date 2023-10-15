042e09651f9aa470c045b5c53ec493a112ec20f1
VanillaIceCreamu <143474072+VanillaIceCreamu@users.noreply.github.com>
2023-10-16 00:26:35 +0900

    pullrequest

---
+++ YS-/Assets/Scripts/Gear.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace vanilla
+{
+    public class Gear : MonoBehaviour
+    {
+        public ItemData.ItemType type;
+        public float rate;
+
+        public void Init(ItemData data)
+        {
+            //Basic Set
+            name = "Gear" + (data.itemId - 2);
+            transform.parent = GameManager.inst.player.transform;
+            transform.localPosition = Vector3.zero;
+            //Property Set
+            type = data.itemType;
+            rate = data.damages[0];
+            ApplyGear();
+        }
+        public void Levelup(float rate)
+        {
+            this.rate = rate;
+            ApplyGear();
+        }
+
+        void ApplyGear()
+        {
+            switch (type)
+            {
+                case ItemData.ItemType.Glove:
+                    RateUp();
+                    break;
+                case ItemData.ItemType.Shoe:
+                    SpeedUp();
+                    break;
+                case ItemData.ItemType.Heal:
+                    break;
+            }
+        }
+        void RateUp()
+        {
+            Weapon[] weapons = transform.parent.GetComponentsInChildren<Weapon>();
+            foreach (Weapon weapon in weapons)
+            {
+                switch (weapon.id)
+                {
+                    case 0:
+                        weapon.speed = weapon.baseSpeed + (weapon.baseSpeed * rate);
+                        break;
+                    case 1:
+                        weapon.speed = 0.5f * (1f - rate);
+                        break;
+                    case 5:
+                        weapon.speed = weapon.baseSpeed - rate * 3;
+                        break;
+                    default:
+                        weapon.speed = weapon.baseSpeed - (weapon.baseSpeed * rate);
+                        break;
+                }
+            }
+        }
+        void SpeedUp()
+        {
+            float speed = 5f;
+            GameManager.inst.player.speed = speed + speed * rate;
+        }
+    }
+}
---
+++ YS-/Assets/Scripts/ItemData.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace vanilla
+{
+    [CreateAssetMenu(fileName = "Item", menuName = "Scriptable Object/ItemData")]
+    public class ItemData : ScriptableObject
+    {
+        public enum ItemType { Melee, Range, Throw, Bounce, Bomb, Glove, Shoe, Heal, Boomerang}
+
+        [Header("# Main Info")]
+        public ItemType itemType;
+        public int itemId;
+        public string itemName;
+
+        [TextArea]
+        public string itemDesc;
+        public Sprite itemIcon;
+
+        [Header("# Level Data")]
+        public float baseDamage;
+        public int baseCount;
+        public float baseSpeed;
+        public float[] damages;
+        public int[] counts;
+
+        [Header("# Weapon")]
+        public GameObject projecttile;
+        public Sprite hand;
+
+    }
+}
